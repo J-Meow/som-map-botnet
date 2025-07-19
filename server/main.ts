e47dbea7af54037bcbfb28c2f61db399b227515d
Jmeow <j@jmeow.net>
2025-07-18 21:51:45 -0500

    Make server return coords for project to go to

--- server/main.ts
+++ server/main.ts
@@ -1,4 +1,25 @@
+const points = [
+    { x: 5, y: 5 },
+    { x: 6, y: 5 },
+    { x: 7, y: 5 },
+]
+const assignments: { [key: string]: number } = {}
+let nextPoint = 0
 Deno.serve({ port: 5218 }, (req) => {
-  console.log(req.url);
-  return new Response("asdf");
-});
+    console.log(req.url)
+    const url = new URL(req.url)
+    if (url.pathname.startsWith("/pointfor/")) {
+        const projectId = url.pathname.split("/pointfor/")[1]
+        let point = { x: 0, y: 0 }
+        if (projectId in assignments) {
+            point = points[assignments[projectId]]
+        } else if (nextPoint in points) {
+            assignments[projectId] = nextPoint++
+            point = points[assignments[projectId]]
+        }
+        return new Response(JSON.stringify({ point }), {
+            headers: { "Content-Type": "application/json" },
+        })
+    }
+    return new Response("asdf")
+})
